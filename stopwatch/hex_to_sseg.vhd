de0da87eb30c9f651e822580e75f8f757dba4233
Petr Kaštánek <48011680+petrkastanek483@users.noreply.github.com>
2019-04-19 11:47:15 +0200

    Update hex_to_sseg.vhd

--- stopwatch/hex_to_sseg.vhd
+++ stopwatch/hex_to_sseg.vhd
@@ -1,8 +1,8 @@
 --------------------------------------------------------------------------------
 -- Brno University of Technology, Department of Radio Electronics
 --------------------------------------------------------------------------------
--- Author: Tomas Fryza (tomas.fryza@vut.cz)
--- Date: 2019-02-12 10:13
+-- Author: Petr Kastanek
+-- Date: 2019-04-18
 -- Design: hex_to_sseg
 -- Description: Hexadecimal digit to seven-segment LED decoder.
 --------------------------------------------------------------------------------
